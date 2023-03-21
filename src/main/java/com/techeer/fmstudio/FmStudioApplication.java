a10b93b3606a31d5cc18c199ae3be81ba03e7109
wschoe <chldntjr0425@naver.com>
2023-03-22 01:00:16 +0900

    initialize project

---
+++ src/main/java/com/techeer/fmstudio/FmStudioApplication.java
@@ -0,0 +1,13 @@
+package com.techeer.fmstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmStudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmStudioApplication.class, args);
+    }
+
+}
