1224e716fe103ddd73c9fe4b722f3a3e2f017b26
baejunil <bjo6300@naver.com>
2023-03-24 13:42:29 +0900

    Feat : task Entity 생성

---
+++ src/main/java/com/techeer/fmstudio/domain/task/entity/Task.java
@@ -0,0 +1,68 @@
+package com.techeer.fmstudio.domain.task.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+import com.sun.istack.NotNull;
+import com.techeer.fmstudio.global.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "task")
+public class Task extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id")
+    private Long id;
+
+    @Column(nullable = false)
+    @NotBlank
+    private String writer;
+
+    @Column(nullable = false)
+    @NotBlank
+    private String title;
+
+    @Column(nullable = false)
+    private String memo;
+
+    @Column(nullable = false)
+    @NotBlank
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime startAt;
+
+    @Column(nullable = false)
+    @NotBlank
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime endAt;
+
+    @Column(nullable = false)
+    @NotBlank
+    private Boolean isFinished;
+
+    @Column(nullable = false)
+    @NotBlank
+    private Boolean isOpened;
+
+    @Builder
+    public Task(String writer, String title, String memo, LocalDateTime startAt, LocalDateTime endAt,
+                Boolean isFinished, Boolean isOpened) {
+        this.writer = writer;
+        this.title = title;
+        this.memo = memo;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.isFinished = isFinished;
+        this.isOpened = isOpened;
+    }
+}
