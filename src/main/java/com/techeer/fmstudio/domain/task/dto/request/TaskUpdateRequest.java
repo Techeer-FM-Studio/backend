b06ddba4d7ebafe8c7ac6d1cd1dca3e1d668bef4
baejunil <bjo6300@naver.com>
2023-04-03 21:31:04 +0900

    feat : 일정 수정 기능

---
+++ src/main/java/com/techeer/fmstudio/domain/task/dto/request/TaskUpdateRequest.java
@@ -0,0 +1,33 @@
+package com.techeer.fmstudio.domain.task.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@AllArgsConstructor
+public class TaskUpdateRequest {
+
+    @NotBlank
+    private Long taskId;
+    @NotBlank
+    private String title;
+    @NotBlank
+    private String memo;
+    @NotBlank
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime startAt;
+    @NotBlank
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime endAt;
+    @NotNull
+    private Boolean isFinished;
+    @NotNull
+    private Boolean isOpened;
+}
