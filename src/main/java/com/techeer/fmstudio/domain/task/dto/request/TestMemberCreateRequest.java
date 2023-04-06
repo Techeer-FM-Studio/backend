f519f51af1157fe8c0bb80bd3ae6a6d0aa75926c
baejunil <bjo6300@naver.com>
2023-04-06 20:35:27 +0900

    feat : TestMemberCreateRequest 유효성 검사

--- src/main/java/com/techeer/fmstudio/domain/task/dto/request/TestMemberCreateRequest.java
+++ src/main/java/com/techeer/fmstudio/domain/task/dto/request/TestMemberCreateRequest.java
@@ -4,13 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Builder
 @AllArgsConstructor
 public class TestMemberCreateRequest {
+    @NotBlank
     private String loginId;
+    @NotBlank
     private String loginPassword;
-
+    @NotBlank
     private String nickname;
     private String status;
     private String introduction;
