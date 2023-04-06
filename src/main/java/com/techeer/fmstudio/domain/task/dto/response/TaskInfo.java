60ab73747001e12e999da9f148d991f595cd7d03
baejunil <bjo6300@naver.com>
2023-04-06 20:31:26 +0900

    fix : TaskInfo에서 유효성 검사 삭제

--- src/main/java/com/techeer/fmstudio/domain/task/dto/response/TaskInfo.java
+++ src/main/java/com/techeer/fmstudio/domain/task/dto/response/TaskInfo.java
@@ -13,26 +13,11 @@ import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor
 public class TaskInfo {
-    @NotBlank
     private String writer;
-
-    @NotBlank
     private String title;
-
-    @NotBlank
     private String memo;
-
-    @NotNull
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime startAt;
-
-    @NotNull
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime endAt;
-
-    @NotNull
     private Boolean isFinished;
-
-    @NotNull
     private Boolean isOpened;
 }
