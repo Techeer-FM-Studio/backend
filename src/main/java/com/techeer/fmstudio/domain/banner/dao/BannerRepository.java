e449026e5b6155ad7f8941792e1f3569c30faa8a
wschoe <chldntjr0425@naver.com>
2023-04-26 03:19:58 +0900

    fix: fix SePL in JPQL grammar

--- src/main/java/com/techeer/fmstudio/domain/banner/dao/BannerRepository.java
+++ src/main/java/com/techeer/fmstudio/domain/banner/dao/BannerRepository.java
@@ -16,15 +16,15 @@ public interface BannerRepository extends JpaRepository<BannerEntity, Long> {
     Optional<BannerEntity> findById(@Param("bannerId") Long bannerId);
 
     @Query("SELECT b FROM BannerEntity b WHERE b.isActive = true " +
-            "AND LOWER(b.bannerType) = LOWER(#{#type.name()})" +
+            "AND LOWER(b.bannerType) = LOWER(:#{#type.name()})" +
             "ORDER BY b.createdAt asc")
     Page<BannerEntity> findBannerByTypeWithPagination(Pageable pageable, @Param("type") BannerType type);
 
-    @Query("UPDATE BannerEntity b SET b.title = #{#entity.title}," +
-            "b.memo = #{#entity.memo}," +
-            "b.startAt = #{entity.startAt}," +
-            "b.endAt = #{entity.endAt} " +
+    @Query("UPDATE BannerEntity b SET b.title = :#{#entity.title}," +
+            "b.memo = :#{#entity.memo}," +
+            "b.startAt = :#{entity.startAt}," +
+            "b.endAt = :#{entity.endAt} " +
             "WHERE b.isActive = true " +
-            "AND b.id = #{#entity.id}")
+            "AND b.id = :#{#entity.id}")
     void updateById(@Param("entity") BannerEntity entity);
 }
