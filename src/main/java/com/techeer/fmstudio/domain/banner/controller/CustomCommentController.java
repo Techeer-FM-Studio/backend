0599644d507e3f2a45ac1c2efad5dac1f6fd1c12
wschoe <chldntjr0425@naver.com>
2023-04-27 06:33:47 +0900

    feat: 커스텀 배너 댓글 불러오기 (페이지네이션)

--- src/main/java/com/techeer/fmstudio/domain/banner/controller/CustomCommentController.java
+++ src/main/java/com/techeer/fmstudio/domain/banner/controller/CustomCommentController.java
@@ -4,6 +4,7 @@ import com.techeer.fmstudio.domain.banner.domain.CommentEntity;
 import com.techeer.fmstudio.domain.banner.dto.mapper.CommentMapper;
 import com.techeer.fmstudio.domain.banner.dto.request.CommentCreateRequest;
 import com.techeer.fmstudio.domain.banner.dto.response.CommentInfo;
+import com.techeer.fmstudio.domain.banner.dto.response.CommentPageInfo;
 import com.techeer.fmstudio.domain.banner.service.CustomCommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,12 +15,12 @@ import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/banners/custom")
+@RequestMapping("api/v1/banners/custom/{bannerId}/comments")
 public class CustomCommentController {
 
     private final CustomCommentService commentService;
     private final CommentMapper commentMapper;
-    @PostMapping("/{bannerId}/comments")
+    @PostMapping
     public ResponseEntity<CommentInfo> create(
             @Valid @RequestBody CommentCreateRequest request,
             @PathVariable Long bannerId
@@ -28,10 +29,21 @@ public class CustomCommentController {
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(commentMapper.toInfo(newComment));
+                .body(commentMapper.toCommentInfo(newComment));
     }
 
-    @DeleteMapping("/{bannerId}/comments/{commentId}")
+    @GetMapping("/page")
+    public ResponseEntity<CommentPageInfo> getCustomBannerCommentByPagination(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "6") int size,
+            @PathVariable Long bannerId
+    ){
+        CommentPageInfo foundCommentList = commentService.getCustomBannerCommentByPagination(bannerId, page, size);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(foundCommentList);
+    }
+
+    @DeleteMapping("/{commentId}")
     public ResponseEntity<String> delete(@PathVariable Long commentId){
         commentService.delete(commentId);
         return ResponseEntity.status(HttpStatus.OK)
--- src/main/java/com/techeer/fmstudio/domain/banner/dao/CommentRepository.java
+++ src/main/java/com/techeer/fmstudio/domain/banner/dao/CommentRepository.java
@@ -1,7 +1,18 @@
 package com.techeer.fmstudio.domain.banner.dao;
 
 import com.techeer.fmstudio.domain.banner.domain.CommentEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+
+    @Query("SELECT c FROM CommentEntity c WHERE c.banner.id = :bannerId " +
+            "AND c.isActive = true " +
+            "ORDER BY c.createdAt asc")
+    Page<CommentEntity> findCommentEntityByBannerIdWithPagination(
+            Pageable pageable,
+            @Param("bannerId") Long bannerId);
 }
