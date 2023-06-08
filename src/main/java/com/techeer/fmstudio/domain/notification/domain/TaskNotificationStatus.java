8440e166f7eedf25b77be6968ab0c3285149a208
baejunil <bjo6300@naver.com>
2023-06-08 22:15:30 +0900

    feat : 일정 수락 여부 엔티티 생성

--- src/main/java/com/techeer/fmstudio/domain/notification/domain/TaskNotificationStatus.java
+++ src/main/java/com/techeer/fmstudio/domain/notification/domain/TaskNotificationStatus.java
@@ -1,7 +1,10 @@
 package com.techeer.fmstudio.domain.notification.domain;
 
+import com.techeer.fmstudio.domain.task.domain.SharedMember;
 import com.techeer.fmstudio.domain.task.domain.Task;
+import com.techeer.fmstudio.global.BaseEntity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,7 +14,7 @@ import javax.persistence.*;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "task_notification_status")
-public class TaskNotificationStatus {
+public class TaskNotificationStatus extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "task_notification_status_id")
@@ -19,11 +22,22 @@ public class TaskNotificationStatus {
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "task_notification_id", nullable = false)
-    private TaskNotification task;
+    private TaskNotification taskNotification;
 
     @Column(nullable = false)
     private Boolean acceptStatus;
 
     @Column(nullable = false)
     private Boolean readStatus;
+
+    @Builder
+    public TaskNotificationStatus(TaskNotification taskNotification, Boolean acceptStatus, Boolean readStatus) {
+        this.taskNotification = taskNotification;
+        this.acceptStatus = acceptStatus;
+        this.readStatus = readStatus;
+    }
+
+    public void deleteTaskNotificationStatus() {
+        this.delete();
+    }
 }
